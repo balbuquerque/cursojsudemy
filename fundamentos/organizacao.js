58679099aac8af1d2f2ac107e52100523f79bd05
Bruno Albuquerque <brunofa1998@gmail.com>
2019-09-24 16:02:20 -0300

    aula de organizacao de codigos em js

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+// organizações de códigos em js, podem ser feitos em sentenças de código ou blocos de código.
+
+// sentenças de código podem ou não serem terminadas em ;
+console.log("Sentença de código")
+
+// organização de bloco; bloco dentro de bloco
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // padrão do curso
+    }
+}
